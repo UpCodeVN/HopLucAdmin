69738b3592c33ae2a56d0232f59533b19e942603
van phuc <vanphucnguyenqn@gmail.com>
2017-12-25 16:03:35 +0700

    Giao dien dang nhap

---
+++ HopLucAdmin/Controllers/Nguoi_Dung_DNController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HopLucAdmin.Controllers
+{
+    public class Nguoi_Dung_DNController : Controller
+    {
+        // GET: Nguoi_Dung_DN
+        public ActionResult Dang_Nhap()
+        {
+            return View();
+        }
+    }
+}
