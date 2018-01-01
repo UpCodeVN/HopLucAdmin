f64d01f675f2fe6e6e4eaee9fb4e10f588305c3f
van phuc <vanphucnguyenqn@gmail.com>
2018-01-01 11:48:32 +0700

    TT Nhan vien

---
+++ HopLucAdmin/Controllers/NhanVienBHController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HopLucAdmin.Models;
+
+
+namespace HopLucAdmin.Controllers
+{
+    public class NhanVienBHController : Controller
+    {
+        HopLucShopEntities db =new  HopLucShopEntities();
+        // GET: NhanVienBH
+        
+        public ActionResult TTNhanVien()
+        {
+            var em = (from nv in db.NhanVienBH orderby nv.ID_NV  select nv);
+            return View(em);
+        }
+
+    }
+}
